5534dd4cd1c947065c0507fe37f448ba8010fce4
a-know <a.know.3373@gmail.com>
2014-10-07 13:52:05 +0900

    Remove HelloWorld resource

--- routes/routes.go
+++ routes/routes.go
@@ -4,11 +4,9 @@ import (
 	"net/http"
 
 	"controller"
-	"hello"
 )
 
 func init() {
-	http.HandleFunc("/", hello.Handler)
 	http.HandleFunc("/postGuestbook", controller.WriteToGuestbook)
 	http.HandleFunc("/getGuestbook", controller.GetMessageList)
 }
